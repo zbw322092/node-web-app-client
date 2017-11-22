872081a535cf4dc92602c872fd4bdf3632759cb3
Bowen Zhong <bz4e14@soton.ac.uk>
2017-11-23 00:21:39 +0800

    header, with some style problems

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,16 +3,24 @@ import { Link } from 'inferno-router';
 import './header.less';
 
 export default class Header extends Component<any, any> {
+
   render() {
     return (
       <div className="header">
+        <div className="company-logo"></div>
         <nav>
-          <a><Link to="/">首页</Link></a>
-          <a><Link to="/product">产品</Link></a>
-          <a><Link to="/service">服务</Link></a>
-          <a><Link to="/about">关于我们</Link></a>
-          <a><Link to="/joinus">招贤纳士</Link></a>
-          <a><Link to="/contact">联系我们</Link></a>
+          <a><Link activeClassName="selected" to="/">首页<div className="hover-underline"></div></Link></a>
+          <a><Link activeClassName="selected" to="/product">产品<div className="hover-underline"></div></Link></a>
+          <a><Link activeClassName="selected" to="/service">服务<div className="hover-underline"></div></Link></a>
+          <a><Link activeClassName="selected" to="/about">关于我们<div className="hover-underline"></div></Link></a>
+          <a><Link activeClassName="selected" to="/joinus">招贤纳士<div className="hover-underline"></div></Link></a>
+          <a><Link activeClassName="selected" to="/contact">联系我们<div className="hover-underline"></div></Link></a>
+
+          <span class="lang">
+            <a className="lang-cn blue">中文</a>
+            <a className="lang-en">/ENGLISH</a>
+          </span>
+          <a className="login">登录</a>
         </nav>
       </div>
     );
