b32f6ec5782d6aa1d207d3635e8ca1f7e353e2f7
Bowen Zhong <bz4e14@soton.ac.uk>
2017-11-23 10:14:44 +0800

    working header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,12 +9,12 @@ export default class Header extends Component<any, any> {
       <div className="header">
         <div className="company-logo"></div>
         <nav>
-          <a><Link activeClassName="selected" to="/">首页<div className="hover-underline"></div></Link></a>
-          <a><Link activeClassName="selected" to="/product">产品<div className="hover-underline"></div></Link></a>
-          <a><Link activeClassName="selected" to="/service">服务<div className="hover-underline"></div></Link></a>
-          <a><Link activeClassName="selected" to="/about">关于我们<div className="hover-underline"></div></Link></a>
-          <a><Link activeClassName="selected" to="/joinus">招贤纳士<div className="hover-underline"></div></Link></a>
-          <a><Link activeClassName="selected" to="/contact">联系我们<div className="hover-underline"></div></Link></a>
+          <Link activeClassName="selected" to="/">首页<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" to="/product">产品<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" to="/service">服务<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" to="/about">关于我们<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" to="/joinus">招贤纳士<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" to="/contact">联系我们<div className="hover-underline"></div></Link>
 
           <span class="lang">
             <a className="lang-cn blue">中文</a>
