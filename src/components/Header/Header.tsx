0244f54c3dc9edf4d1fc4406ad007c91237a4ed6
Bowen Zhong <bz4e14@soton.ac.uk>
2017-12-03 18:13:58 +0800

    responsive header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,24 +4,49 @@ import './header.less';
 
 export default class Header extends Component<any, any> {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+  }
+
+  menuToggle = () => {
+    this.setState({
+      menuOpen: !this.state.menuOpen
+    });
+  }
+
+  navHandler = () => {
+    this.setState({
+      menuOpen: false
+    });
+  }
+
   render() {
     return (
       <div className="header">
         <div className="company-logo"></div>
-        <nav>
-          <Link activeClassName="selected" to="/">首页<div className="hover-underline"></div></Link>
-          <Link activeClassName="selected" to="/product">产品<div className="hover-underline"></div></Link>
-          <Link activeClassName="selected" to="/service">服务<div className="hover-underline"></div></Link>
-          <Link activeClassName="selected" to="/about">关于我们<div className="hover-underline"></div></Link>
-          <Link activeClassName="selected" to="/joinus">招贤纳士<div className="hover-underline"></div></Link>
-          <Link activeClassName="selected" to="/contact">联系我们<div className="hover-underline"></div></Link>
+        <nav className={this.state.menuOpen ? 'show-toggle-nav': ''}>
+          <Link activeClassName="selected" onClick={this.navHandler} to="/">首页<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" onClick={this.navHandler} to="/product">产品<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" onClick={this.navHandler} to="/service">服务<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" onClick={this.navHandler} to="/about">关于我们<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" onClick={this.navHandler} to="/joinus">招贤纳士<div className="hover-underline"></div></Link>
+          <Link activeClassName="selected" onClick={this.navHandler} to="/contact">联系我们<div className="hover-underline"></div></Link>
 
           <span class="lang">
-            <a className="lang-cn blue">中文</a>
-            <a className="lang-en">/ENGLISH</a>
+            <a className="lang-cn blue" onClick={this.navHandler}>中文</a>
+            <a className="lang-en" onClick={this.navHandler}>/ENGLISH</a>
           </span>
-          <a className="login">登录</a>
+          <a className="login" onClick={this.navHandler}>登录</a>
         </nav>
+
+        <a href="#" className={this.state.menuOpen ? `custom-menu-toggle x` : `custom-menu-toggle`} 
+          id="toggle" onClick={this.menuToggle}>
+          <s class="bar"></s>
+          <s class="bar"></s>
+        </a>
       </div>
     );
   }
